0a0940e8a3fc2944ecb9d2d219d3e1cb573e3535
dantsub <dany.mrtinez@gmail.com>
2020-07-19 16:09:58 -0500

    [Fix] correct some test cases

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -14,11 +14,15 @@ def canUnlockAll(boxes):
     # check if boxes and sub boxes are a list
     if type(boxes) is not list or not all(type(box) is list for box in boxes):
         return False
+    # check if list is empty
+    if len(boxes) == 0:
+        return False
+    # check if only exist one box
     if len(boxes) == 1:
         return True
     # check if first box is empty
-    if not boxes[0] and len(boxes) == 1:
-        return True
+    if not boxes[0] and len(boxes) > 1:
+        return False
     # dictionary of all boxes, all boxes are lock here
     unlock = {k: False for k in range(len(boxes))}
     # unlock first box
@@ -29,9 +33,9 @@ def canUnlockAll(boxes):
     while keys:
         new_k = []
         for key in keys:
-            if unlock[key] is False:
+            if key in unlock.keys() and unlock[key] is False:
                 new_k += boxes[key]
-            unlock[key] = True
+                unlock[key] = True
         # If all boxes unlock return True
         if all(unlock.values()) and len(unlock) == len(boxes):
             return True
