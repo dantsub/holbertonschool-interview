d47c5d1116c9e2e48b78cdee0de17ff87deca703
dantsub <dany.mrtinez@gmail.com>
2020-08-31 19:03:43 -0500

    [Fix] add main condition

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,33 +4,33 @@
 from sys import stdin
 
 
-codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-size = 0
+if __name__ == '__main__':
+    codes = {'200': 0, '301': 0, '400': 0, '401': 0,
+             '403': 0, '404': 0, '405': 0, '500': 0}
+    size = 0
 
+    def print_info(codes, size):
+        """print_info method print needed info
 
-def print_info(codes, size):
-    """print_info method print needed info
+        Args:
+            codes (dict): code status
+            size (int): size of files
+        """
+        print("File size: {}".format(size))
+        for key, val in sorted(codes.items()):
+            if val > 0:
+                print("{}: {}".format(key, val))
 
-    Args:
-        codes (dict): code status
-        size (int): size of files
-    """
-    print("File size: {}".format(size))
-    for key, val in sorted(codes.items()):
-        if val > 0:
-            print("{}: {}".format(key, val))
-
-try:
-    for i, line in enumerate(stdin, 1):
-        line = line[-9:]
-        info = line.split()
-        if info[-2] in codes.keys():
-            codes[info[-2]] += 1
-            size += int(info[-1])
-        if not i % 10:
-            print_info(codes, size)
-except KeyboardInterrupt:
+    try:
+        for i, line in enumerate(stdin, 1):
+            line = line[-9:]
+            info = line.split()
+            if info[-2] in codes.keys():
+                codes[info[-2]] += 1
+                size += int(info[-1])
+            if not i % 10:
+                print_info(codes, size)
+    except KeyboardInterrupt:
+        print_info(codes, size)
+        raise
     print_info(codes, size)
-    raise
-print_info(codes, size)
