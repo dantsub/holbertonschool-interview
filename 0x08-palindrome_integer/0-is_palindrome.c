ec3594b54dcc7d0d263e2a4eac593948b3d869bd
dantsub <dany.mrtinez@gmail.com>
2020-09-15 17:31:05 -0500

    [Update] simplifying

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -8,23 +8,11 @@
 int is_palindrome(unsigned long n)
 {
 	unsigned long aux = 0, n1 = n;
-	int mid = 0, first = 0, last = 0;
 
 	while (n1)
 	{
 		aux = (aux * 10) + (n1 % 10);
-		n1 /= 10, mid++;
+		n1 /= 10;
 	}
-	n1 = n;
-	mid = (mid / 2) + (mid % 2);
-	while (mid)
-	{
-		last = n1 % 10;
-		first = aux % 10;
-		if (first != last)
-			return (0);
-		n1 /= 10, aux /= 10;
-		mid--;
-	}
-	return (1);
+	return (n == aux ? 1 : 0);
 }
