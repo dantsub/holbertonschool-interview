40ee67945a4cea0f3161451f3d820bf76cd30739
dantsub <dany.mrtinez@gmail.com>
2021-03-14 15:16:05 -0500

    [Update] change a little bit logic

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -11,9 +11,6 @@ def island_perimeter(grid):
         grid ([[int]]): is a list of list of integers
     Return: perimeter of the island described in grid
     """
-    if len(grid) < 1:
-        return
-
     _sum, limit = (0, 0)
     len_grid, len_col = (len(grid), len(grid[0]))
 
@@ -21,7 +18,9 @@ def island_perimeter(grid):
         _sum += sum(grid[col])
         for row in range(len_col):
             if grid[col][row]:
-                if ((row > 0 and grid[col][row - 1] == 1) or
-                   (col > 0 and grid[col - 1][row] == 1)):
+                if row > 0 and grid[col][row - 1] == 1:
+                    limit += 1
+
+                if col > 0 and grid[col - 1][row] == 1:
                     limit += 1
     return _sum * 4 - limit * 2
